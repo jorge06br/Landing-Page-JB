8f063ed9ae648632f230e7b548ee2378a23e6e9e
jorge06br <jorgebaca06@gmail.com>
2022-06-20 03:13:48 +0000

    [CARTAS GENERADAS CON INFO]

--- src/js/component/Card.js
+++ src/js/component/Card.js
@@ -1,25 +1,27 @@
-import proptypes from 'prop-types';
-import React from 'react';
-const Card=(props) =>{
-    return (
-        <div className="card m-5">
-            <img className="card-img-top" src={props.cardImg} alt="Card image cap" />
-            <div className="card-body">
-                <h5 className="card-title">{props.cardTitle}</h5>
-                <p className="card-text">{props.cardDescription}</p>
-                <a href={props.cardBtnUrl} className="btn btn-primary">
-                    {props.cardLabel}
-                </a>
-            </div>
-        </div>
-    );
-}
-Card.proptypes = {
-	cardImg: proptypes.string,
-	cardTitle: proptypes.string,
-	cardDescription: proptypes.string,
-	cardBtnUrl: proptypes.string,
-	cardLabel: proptypes.string
-}
+import React from "react";
+
+const Card = (alfa) => {
+  return (
+    <div className="card m-5">
+      <img
+        className="card-img-top cardStyle"
+        src={alfa.ImgURL}
+        alt="Card image cap"
+      />
+      <div className="card-body">
+        <h5 className="card-title">{alfa.Title}</h5>
+        <p className="card-text">{alfa.Description}</p>
+        <a href={alfa.BtnURL} className="btn btn-primary">
+          {alfa.BtnLabel}
+        </a>
+      </div>
+    </div>
+  );
+};
+
+const cardStyle = {
+  width: "500px",
+  height: "325px",
+};
 
 export default Card;
